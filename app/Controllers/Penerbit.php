809f4867cf3f9f74d4146db98a1f994426f3becb
umihabibah222 <umihabjibah@gmail.com>
2024-11-20 23:12:59 +0700

    add commit

--- app/Controllers/Penerbit.php
+++ app/Controllers/Penerbit.php
@@ -25,4 +25,32 @@ class Penerbit extends BaseController
         ];
         return view('v_template_admin',$data);
     }
+
+    public function AddData()
+    {
+        $data= ['nama_penerbit'=> $this->request->getPost('nama_penerbit')];
+        $this->ModelPenerbit->AddData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Ditambah');
+        return redirect()->to(base_url('Penerbit'));
+    }
+
+    public function EditData($id_penerbit)
+    {
+        $data= [
+            'id_penerbit' => $id_penerbit,
+            'nama_penerbit'=> $this->request->getPost('nama_penerbit')
+        ];
+        $this->ModelPenerbit->EditData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Di Update!');
+        return redirect()->to(base_url('Penerbit'));
+    }
+
+    public function DeleteData( $id_penerbit)
+    {
+        $data= ['id_penerbit'=>  $id_penerbit];
+        $this->ModelPenerbit->DeleteData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil DiHapus !');
+        return redirect()->to(base_url('Penerbit'));
+    }
+
 }
